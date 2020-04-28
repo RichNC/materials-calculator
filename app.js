7c5ecb1498ac5263637eac1c1a01b4649b5af94c
Rich Cherry <rich.m.cherry@gmail.com>
2020-04-27 20:23:16 -0400

    added numbers separator

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+
+
 const casesNeeded = () => {
   const length = document.getElementById('length').value;
   const height = document.getElementById('height').value;
-  const area = length * height;
+  const unitOfMeasurement = document.getElementById('unitOfMeasurement').value;
+  let area = '';
+  if (unitOfMeasurement === 'feet') {
+    area = (length * height);
+  } else if (unitOfMeasurement === 'inches') {
+    area = (length/144 * height/144);
+  }
   const cases = Math.round(area / 24);
-  document.getElementById('resultSentence').innerText=`The area of your space is ${area} feet. You will need ${cases} cases.`
-}
 
+
+  document.getElementById('resultSentence').innerText=`The area of your space is ${area.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} feet. You will need ${cases.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')} cases.`
+}
 
